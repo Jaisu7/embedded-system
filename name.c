11b5413fe213d3a51022092f533fb2cf4a2e5b8d
Jaise Biju <jaisebiju07@gmail.com>
2025-08-19 16:46:15 +0530

    edited name.c file

--- name.c
+++ name.c
@@ -1,9 +1,8 @@
-// Online C compiler to run C program online
 #include <stdio.h>
 
 int main() {
     // Write C code here
-    printf("hi");
-
+    printf("hi jaise lets start the coding\n");
+    printf("embeded ");
     return 0;
 }
